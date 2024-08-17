79bd90d1fed748596f0a0b80c4799df924c73e0d
Sakuya133 <55240860+Sakuya133@users.noreply.github.com>
2024-08-17 21:16:32 +0700

    Create latihan.cpp

---
+++ latihan.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    string a;
+
+    cin >> a;
+/*halo nama aku yuki suou*/
+
+    int cek =a.size();
+
+    int iya=0;
+    for(int i=0;i<cek;i++){
+        if(a[i]=='4' ||a[i]=='7' ){
+            iya++;
+        } else {
+            cout << "NO";
+            return 0;
+        }
+    }
+
+    if(iya == 4 || iya == 7){
+        cout << "YES";
+    } else{
+        cout << "NO";
+    }
+
+    
+}s
